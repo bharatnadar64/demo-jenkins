a42f45e1b8c85bf8638e5d1e68613fc4f618cc86
Bharat Nadar <bharatnadar64@gmail.com>
2026-03-08 23:24:02 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class Sample{
    public static void main(String[] args) {
        System.out.println("Sample Java file executed from repo");
+             System.out.println("New Line for Sample Java file executed from repo");
    }
 }
